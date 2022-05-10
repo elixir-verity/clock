383ffab4512230ce28b86bbafb259eb866d48c77
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2022-05-09 21:23:14 -0400

    Correct namespace

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule VerityClock.MixProject do
+defmodule Verity.Clock.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :verity_clock,
+      app: :vrt_clock,
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
